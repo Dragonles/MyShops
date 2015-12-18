35cc5e12696a9bc01eee8390794e34974990002f
Dragonles <981506405@qq.com>
2015-12-18 13:49:19 +0800

    add null

---
+++ app/src/main/java/com/myshops/shops/myshops/activity/OrderFormActivity.java
@@ -0,0 +1,15 @@
+package com.myshops.shops.myshops.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myshops.shops.myshops.R;
+
+public class OrderFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_form);
+    }
+}
