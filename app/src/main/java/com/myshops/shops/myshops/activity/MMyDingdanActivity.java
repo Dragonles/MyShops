7d53cfa4a076c8e59229e7894128daae19a52115
likun125125 <1181985131@qq.com>
2015-12-18 15:58:52 +0800

    add there

---
+++ app/src/main/java/com/myshops/shops/myshops/activity/MMyDingdanActivity.java
@@ -0,0 +1,13 @@
+package com.myshops.shops.myshops.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MMyDingdanActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mmy_dingdan);
+    }
+}
