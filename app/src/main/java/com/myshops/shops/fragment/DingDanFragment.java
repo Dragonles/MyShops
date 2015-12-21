ec771aa8437139abe1db3e1b513f8df7e87afa9b
likun125125 <1181985131@qq.com>
2015-12-21 10:44:30 +0800

    no message

--- app/src/main/java/com/myshops/shops/fragment/DingDanFragment.java
+++ app/src/main/java/com/myshops/shops/fragment/DingDanFragment.java
@@ -1,16 +1,21 @@
 package com.myshops.shops.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.myshops.shops.myshops.R;
+import com.myshops.shops.myshops.ShopInfoActivity;
 
 public class DingDanFragment extends Fragment {
+
+    Button btn_tiaozhuan;
 
     public DingDanFragment() {
         // Required empty public constructor
@@ -25,6 +30,17 @@ public class DingDanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ding_dan, container, false);
+        View v = inflater.inflate(R.layout.fragment_ding_dan, container, false);
+
+        btn_tiaozhuan = (Button) v.findViewById(R.id.btn_tiaozhuan);
+        btn_tiaozhuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ShopInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
